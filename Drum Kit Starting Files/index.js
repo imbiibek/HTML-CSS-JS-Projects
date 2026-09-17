211d2cf9daf9518539eaefe03b2760effe7ceeff
Bibek Yadav <bibekyadav735@gmail.com>
2026-09-17 23:35:03 +0545

    Initial Commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,11 @@
+document.querySelector('.set').addEventListener("click", handleClick);
+
+
+function handleClick() {
+
+  document.querySelector('.set').style.color = "white";
+
+}
+
+//  var audio = new Audio('sounds/tom-1.mp3')
+//    audio.play();
